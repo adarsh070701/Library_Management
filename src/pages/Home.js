2114999c53aef78be5a817db0013107326b21991
adarsh070701 <adarshrajakooo@gmail.com>
2023-08-14 16:06:22 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export default function Home() {
+    const [users, setUsers]= useState([]);
+    useEffect(() => {
+        loadUsers()
+    }, []);
+    const loadUsers=async()=>{
+        const result=await axios.get("http://localhost:8080/users")
+        setUsers(result.data);
+    }
+
+  return (
+    <div className='container'>
+        <div className='py-4'>
+        <table className="table border shadow">
+  <thead>
+    <tr>
+      <th scope="col">S.N.</th>
+      <th scope="col">Name</th>
+      <th scope="col">User Name</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+        users.map((user,index)=>(
+            <tr>
+            <th scope="row" key={index}>{index+1}</th>
+            <td>{users.name}</td>
+            <td>{users.username}</td>
+            <td>{users.email}</td>
+            </tr>
+        ))
+    }
+    
+  </tbody>
+</table>
+        </div>
+    </div>
+  )
+}
